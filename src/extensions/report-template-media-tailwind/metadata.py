988a61c270d12dbbc7bf4e03d94ffde85d084112
Eduardo Aguiar <aguiar@protonmail.ch>
2026-07-02 17:32:33 -0300

    report-template-media-tailwind: Created new extension report-template-media-tailwind: Created Generator class

---
+++ src/extensions/report-template-media-tailwind/metadata.py
@@ -0,0 +1,26 @@
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Mobius Forensic Toolkit
+# Copyright (C) 2008-2026 Eduardo Aguiar
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Extension metadata
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+EXTENSION_ID = 'report-template-media-tailwind'
+EXTENSION_NAME = 'Report Template: Media TailWind CSS'
+EXTENSION_AUTHOR = 'Eduardo Aguiar'
+EXTENSION_VERSION = '1.0'
+EXTENSION_DESCRIPTION = 'Report Template for Media using TailWind CSS'
